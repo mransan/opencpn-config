2bd6fcaf7ca137e551a20e6e9b3d5c78f1cf63e3
Maxime Ransan <maxime.ransan@gmail.com>
2017-12-29 01:32:59 +0000

    Add some packages

---
+++ package_install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install libgnuradio-analog
+sudo apt-get install gnuradio-dev
+sudo apt-get install gr-osmosdr
+sudo apt-get install libqt5svg5-dev
+sudo apt-get install libboost-dev
+sudo apt-get install liblog4cpp5-dev
